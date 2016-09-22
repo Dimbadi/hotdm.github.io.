5679cd9876ee4fc2759ddd20bea4c688de953cbb
Dimbadi <malenki_di@mail.ru>
2016-09-22 15:48:25 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,164 @@
+$(document).ready(function() {
+
+	// Валидация формы
+	$("form").validate({
+        submitHandler: function(form) {
+            $.ajax({
+                type: "GET",
+                url: "mail.php",
+                data: $("form").serialize()
+            }).done(function() {
+                $("#success").fancybox().trigger('click');
+                $("form")[0].reset();
+                setTimeout(function() {
+                    $.fancybox.close();
+                }, 3000);
+            });
+        },
+       rules:{ 
+        
+            name:{
+                required: true,
+                minlength: 4,
+                maxlength: 50,
+            },
+            tel:{
+                minlength: 7,
+                maxlength: 17,
+                required: true,
+            },
+            email:{
+                email: true,
+                required: true,
+            },
+       },
+       messages:{
+        
+            name:{
+                required: "* это поле обязательно для заполнения",
+                minlength: "* имя должно быть минимум 4 символа",
+                maxlength: "* максимальное число символо - 50",
+            },
+
+            tel:{
+                required: "* это поле обязательно для заполнения",
+                minlength: "* телефон должен быть минимум 7 цифр",
+                maxlength: "* максимальное число цифр - 20",
+            },
+
+            email:{
+                required: "* это поле обязательно для заполнения",
+                email: "* неправильно указан e-mail адрес",
+            },
+       }
+    });
+
+	// Маска ввода телефона в формате +375 (...) ...
+    $('.number-mask')
+
+    .keydown(function (e) {
+        var key = e.charCode || e.keyCode || 0;
+        $phone = $(this);
+
+        // Auto-format- do not expose the mask as the user begins to type
+        if (key !== 8 && key !== 9) {
+            if ($phone.val().length === 8) {
+                $phone.val($phone.val() + ') ');
+            }
+        }
+
+        // Allow numeric (and tab, backspace, delete) keys only
+        return (key == 8 || 
+                key == 9 ||
+                key == 46 ||
+                (key >= 48 && key <= 57) ||
+                (key >= 96 && key <= 105)); 
+    })
+    
+    .bind('focus click', function () {
+        $phone = $(this);
+        
+        if ($phone.val().length === 0) {
+            $phone.val('+375 (');
+        }
+        else {
+            var val = $phone.val();
+            $phone.val('').val(val); // Ensure cursor remains at the end
+        }
+    })
+    
+    .blur(function () {
+        $phone = $(this);
+        
+        if ($phone.val() === '(') {
+            $phone.val('');
+        }
+    });
+
+	//Таймер обратного отсчета
+	//Документация: http://keith-wood.name/countdown.html
+	//<div class="countdown" date-time="2015-01-07"></div>
+	var austDay = new Date($(".countdown").attr("date-time"));
+	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox(); //rel="lightbox" 
+
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".top_mnu").navigation();
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 4
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+});
